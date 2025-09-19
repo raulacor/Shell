e1bd2f87f8ac12d0ced5a9e36e262944925924e7
Raul Azzi Corsi <raulcorsi@Rauls-MacBook.local>
2025-09-19 09:45:18 -0300

    Add: Commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,60 @@
 #[allow(unused_imports)]
-#[allow(unconditional_recursion)]
 use std::io::{self, Write};
 
+const BUILT_IN_COMMANDS: [&str; 3] = ["echo", "exit", "type"];
+enum Command {
+    ExitCommand,
+    EchoCommand { display_string: String },
+    TypeCommand { command_name: String },
+    CommandNotFound,
+}
+
+impl Command {
+    fn from_input(input: &str) -> Self {
+        let input = input.trim();
+        if input == "exit 0" {
+            return Self::ExitCommand;
+        };
+        if let Some(pos) = input.find("echo ") {
+            if pos == 0 {
+                return Self::EchoCommand {
+                    display_string: input["echo ".len()..].to_string(),
+                };
+            }
+        }
+        if let Some(pos) = input.find("type ") {
+            if pos == 0 {
+                return Self::TypeCommand {
+                    command_name: input["type ".len()..].to_string(),
+                };
+            }
+        }
+        Self::CommandNotFound
+    }
+}
+
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    // Uncomment this block to pass the first stage
+
     // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
-    main();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let command = Command::from_input(&input);
+
+        match command {
+            Command::ExitCommand => break,
+            Command::EchoCommand { display_string } => println!("{}", display_string),
+            Command::TypeCommand { command_name } => {
+                if BUILT_IN_COMMANDS.contains(&command_name.as_str()) {
+                    println!("{} is a shell builtin", command_name);
+                } else {
+                    println!("{}: command not found", command_name)
+                }
+            }
+            Command::CommandNotFound => println!("{}: command not found", input.trim()),
+        }
+    }
 }
